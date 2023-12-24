3885066a92a98c3cf10cbbd00fb1e72f96a415fb
EminOsmanbegovic <emin.osmanbegovic@isatis.com>
2023-12-24 21:58:32 +0100

    Refactored componenets to seperate files

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,6 @@
-import reactImage from "./assets/react-core-concepts.png";
-import componentImage from "./assets/components.png";
 import { CORE_CONCEPTS} from "./data.js";
-
-const stringDescriptons = ["Core", "Crucial", "Fundamental"];
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
-}
-
-function Header() {
-  const description = stringDescriptons[getRandomInt(stringDescriptons.length)];
-
-  return (
-    <header>
-      <img src={reactImage} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
-function CoreConcepts({image, title, description}) {
-  return(
-    <li>
-      <img src={image}></img>
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  )
-}
+import Header from "./components/Header.jsx";
+import CoreConcepts from "./components/CoreConcepts.jsx";
 
 function App() {
   return (
---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import reactImage from "../assets/react-core-concepts.png";
+
+const stringDescriptons = ["Core", "Crucial", "Fundamental"];
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+export default function Header() {
+    const description = stringDescriptons[getRandomInt(stringDescriptons.length)];
+  
+    return (
+      <header>
+        <img src={reactImage} alt="Stylized atom" />
+        <h1>React Essentials</h1>
+        <p>
+          {description} React concepts you will need for almost any app you are
+          going to build!
+        </p>
+      </header>
+    );
+  }
